4f42ed858602f4e0d923a3cc97611bb071a8176b
Michael Williams <manicdesigner@gmail.com>
2020-02-26 18:45:09 -0500

    json message line 35 modded and format file

--- routes/acounts-router.js
+++ routes/acounts-router.js
@@ -8,12 +8,12 @@ const validateAccountData = require('../middleware/validatePostAccountData');
  * Description: Selects all accounts in `accounts` in  database.
  */
 router.get('/', async (req, res, next) => {
-	try {
-		const accounts = await db('accounts').select('*');
-		res.status(200).json(accounts);
-	} catch (err) {
-		next(err);
-	}
+  try {
+    const accounts = await db('accounts').select('*');
+    res.status(200).json(accounts);
+  } catch (err) {
+    next(err);
+  }
 });
 
 /**
@@ -23,20 +23,20 @@ router.get('/', async (req, res, next) => {
  * else returns `account` info.
  */
 router.get('/:id', async (req, res, next) => {
-	const { id } = req.params;
-	try {
-		const account = await db('accounts')
-			.where({ id: id })
-			.select('*')
-			.first();
-		if (account) {
-			res.status(200).json(account);
-		} else {
-			res.status(400).json({ message: 'the specific ID was not found' });
-		}
-	} catch (err) {
-		next(err);
-	}
+  const { id } = req.params;
+  try {
+    const account = await db('accounts')
+      .where({ id: id })
+      .select('*')
+      .first();
+    if (account) {
+      res.status(200).json(account);
+    } else {
+      res.status(400).json({ message: 'ID was not found to retrieve' });
+    }
+  } catch (err) {
+    next(err);
+  }
 });
 
 /**
@@ -49,22 +49,22 @@ router.get('/:id', async (req, res, next) => {
  * returning the newly inserted data back w/ a status 201.
  */
 router.post('/', validateAccountData(), async (req, res, next) => {
-	const { name, budget } = req.body;
+  const { name, budget } = req.body;
 
-	const accountPayload = {
-		name: name,
-		budget: budget
-	};
-	try {
-		const [id] = await db('accounts').insert(accountPayload);
-		const addedAccount = await db('accounts')
-			.where({ id: id })
-			.select('*')
-			.first();
-		res.status(201).json(addedAccount);
-	} catch (err) {
-		next(err);
-	}
+  const accountPayload = {
+    name: name,
+    budget: budget,
+  };
+  try {
+    const [id] = await db('accounts').insert(accountPayload);
+    const addedAccount = await db('accounts')
+      .where({ id: id })
+      .select('*')
+      .first();
+    res.status(201).json(addedAccount);
+  } catch (err) {
+    next(err);
+  }
 });
 
 /**
@@ -77,49 +77,49 @@ router.post('/', validateAccountData(), async (req, res, next) => {
  * check if has id 404 not else returns data.
  */
 router.put('/:id', validateAccountData(), async (req, res, next) => {
-	const { id } = req.params;
-	const { name, budget } = req.body;
+  const { id } = req.params;
+  const { name, budget } = req.body;
 
-	const updateAccount = {
-		name: name,
-		budget: budget
-	};
-	try {
-		await db('accounts')
-			.where({ id: id })
-			.update(updateAccount);
-		const newAccount = await db('accounts')
-			.where({ id: id })
-			.select('*')
-			.first();
-		if (newAccount) {
-			res.status(200).json(newAccount);
-		} else {
-			res.status(400).json({ message: 'Could not find specific ID to update' });
-		}
-	} catch (err) {
-		next(err);
-	}
+  const updateAccount = {
+    name: name,
+    budget: budget,
+  };
+  try {
+    await db('accounts')
+      .where({ id: id })
+      .update(updateAccount);
+    const newAccount = await db('accounts')
+      .where({ id: id })
+      .select('*')
+      .first();
+    if (newAccount) {
+      res.status(200).json(newAccount);
+    } else {
+      res.status(400).json({ message: 'Could not find specific ID to update' });
+    }
+  } catch (err) {
+    next(err);
+  }
 });
 
 /**
  * DELETE /api/projects/:id
  */
 router.delete('/:id', async (req, res, next) => {
-	const { id } = req.params;
-	try {
-		const rowsDel = await db('accounts')
-			.where({ id: id })
-			.del();
-		if (rowsDel) {
-			res.status(204).end();
-		} else {
-			res.status(400).json({ message: 'ID not found to delete' });
-		}
-	} catch (err) {
-		console.log(err);
-		next(err);
-	}
+  const { id } = req.params;
+  try {
+    const rowsDel = await db('accounts')
+      .where({ id: id })
+      .del();
+    if (rowsDel) {
+      res.status(204).end();
+    } else {
+      res.status(400).json({ message: 'ID not found to delete' });
+    }
+  } catch (err) {
+    console.log(err);
+    next(err);
+  }
 });
 
 module.exports = router;
