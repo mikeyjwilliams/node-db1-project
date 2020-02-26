31b69ead9f0a33d35e96e86a64d3e7408b0f785e
Michael Williams <manicdesigner@gmail.com>
2020-02-26 09:00:00 -0500

    add 404, 500 server error and test if server is up

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 const express = require('express');
-
 const db = require('./data/dbConfig.js');
-
 const server = express();
+const accountsRouter = require('./routes/acounts-router');
 
 server.use(express.json());
+
+server.get('/', (req, res) => {
+  res.send('accounts is online');
+});
+
+server.use('/api/accounts', accountsRouter);
+
+server.use((req, res) => {
+  res.status(404).json({ message: '404 page not found' });
+});
+
+server.use((err, req, res, next) => {
+  res.status(500).json({ errorMessage: 'Internal server error' });
+});
 
 module.exports = server;
