223aa13d03872fc1c558ebe5b227804b910867f4
Michael Williams <manicdesigner@gmail.com>
2020-02-26 09:35:40 -0500

    GET all accounts

--- routes/acounts-router.js
+++ routes/acounts-router.js
@@ -2,4 +2,13 @@ const express = require('express');
 const db = require('../data/dbConfig');
 const router = express.Router();
 
+router.get('/', async (req, res, next) => {
+  try {
+    const accounts = await db('accounts').select('*');
+    res.status(200).json(accounts);
+  } catch (err) {
+    next(err);
+  }
+});
+
 module.exports = router;
